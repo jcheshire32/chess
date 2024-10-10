14c7ae14a7b97dde20cd9fb35b1000fddadcf115
jcheshire32 <jcheshire32@gmail.com>
2024-10-09 22:07:14 -0600

    Service startup

---
+++ server/src/main/java/dataaccess/ClearDAO.java
@@ -0,0 +1,7 @@
+package dataaccess;
+
+public class ClearDAO {
+    clearUsers() throws DataAccessException{}
+    clearGames() throws DataAccessException{}
+    clearAuth() throws DataAccessException{}
+}
