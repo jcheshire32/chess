9ab4c113eedf90e77e357bdf2bec33537e187136
jcheshire32 <jcheshire32@gmail.com>
2024-11-11 20:43:14 -0700

    Board done

--- client/src/main/java/ui/Board.java
+++ client/src/main/java/ui/Board.java
@@ -22,32 +22,28 @@ public class Board {
     private static final ChessPiece EMPTY = null;
     private static final ChessPiece b_pawn = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN);
     private static final ChessPiece w_pawn = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-//    private static final ChessPiece rook = "r";
-//    private static final ChessPiece knight = "n";
-//    private static final ChessPiece bishop = "b";
-//    private static final ChessPiece queen = "q";
-//    private static final ChessPiece king = "k";
+    private static final ChessPiece b_rook = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK);
+    private static final ChessPiece w_rook = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK);
+    private static final ChessPiece b_knight = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT);
+    private static final ChessPiece w_knight = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT);
+    private static final ChessPiece b_bishop = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP);
+    private static final ChessPiece w_bishop = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP);
+    private static final ChessPiece b_queen = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.QUEEN);
+    private static final ChessPiece w_queen = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.QUEEN);
+    private static final ChessPiece b_king = new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KING);
+    private static final ChessPiece w_king = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KING);
+
 
     private static final ChessPiece[][] boardStart = {
-            {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+            {b_rook, b_knight, b_bishop, b_queen, b_king, b_bishop, b_knight, b_rook},
             {b_pawn, b_pawn, b_pawn, b_pawn, b_pawn, b_pawn, b_pawn, b_pawn},
             {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
             {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
             {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
             {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
             {w_pawn, w_pawn, w_pawn, w_pawn, w_pawn, w_pawn, w_pawn, w_pawn},
-            {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
+            {w_rook, w_knight, w_bishop, w_queen, w_king, w_bishop, w_knight, w_rook}
     };
-//    private static final ChessPiece[][] boardStart = {
-//            {rook, knight, bishop, queen, king, bishop, knight, rook},
-//            {b_pawn, b_pawn, b_pawn, b_pawn, b_pawn, b_pawn, b_pawn, b_pawn},
-//            {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
-//            {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
-//            {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
-//            {EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
-//            {w_pawn, w_pawn, w_pawn, w_pawn, w_pawn, w_pawn, w_pawn, w_pawn},
-//            {rook, knight, bishop, queen, king, bishop, knight, rook}
-//    };
 
     public static void main(String[] args) {
         var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
@@ -153,11 +149,6 @@ public class Board {
         out.print(SET_TEXT_COLOR_WHITE);
     }
 
-    private static void setRed(PrintStream out) {
-        out.print(SET_BG_COLOR_RED);
-        out.print(SET_TEXT_COLOR_RED);
-    }
-
     private static void setBlack(PrintStream out) {
         out.print(SET_BG_COLOR_BLACK);
         out.print(SET_TEXT_COLOR_BLACK);
@@ -169,13 +160,36 @@ public class Board {
     }
 
     private static void printPlayer(PrintStream out, ChessPiece player) {
-        //translate piecetype to letter. Could do a map
+        //translate piecetype to letter. map or switch case
         if (player == null){
             out.print(" ");
+            return;
         } else if (player.getTeamColor() == ChessGame.TeamColor.BLACK) {
             out.print(SET_TEXT_COLOR_GREEN);
         } else {
             out.print(SET_TEXT_COLOR_RED);
+        }
+        switch (player.getPieceType()) {
+            case KING:
+                out.print("K");
+                break;
+            case QUEEN:
+                out.print("Q");
+                break;
+            case BISHOP:
+                out.print("B");
+                break;
+            case KNIGHT:
+                out.print("N");
+                break;
+            case PAWN:
+                out.print("P");
+                break;
+            case ROOK:
+                out.print("R");
+                break;
+            default:
+                break;
         }
     }
 }
